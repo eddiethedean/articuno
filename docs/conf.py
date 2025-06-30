83b08467492b4ea6ed4764d364b9eee7147f98a8
Odos Matthews <omatth10@wgu.edu>
2025-06-30 16:01:22 -0400

    update docs:

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
-    "sphinx-autodoc-typehints",
+    "sphinx_autodoc_typehints",
 ]
 
 templates_path = ["_templates"]
