0307f8ca848b0cc8878449d2758878381823c522
Odos Matthews <omatth10@wgu.edu>
2025-06-30 16:04:42 -0400

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -7,10 +7,11 @@ author = "Odos Matthews"
 release = "0.3.7"
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
-    "sphinx_autodoc_typehints",
+'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
 ]
 
 templates_path = ["_templates"]
@@ -18,3 +19,14 @@ exclude_patterns = []
 
 html_theme = "furo"
 html_static_path = ["_static"]
+
+
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+    'inherited-members': True,
+    'no-index': True,  # This tells Sphinx not to index members separately
+}
+
+typehints_fully_qualified = True
