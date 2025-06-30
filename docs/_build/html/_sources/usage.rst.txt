44bebfe976de37850f19eb267b17dffbd415a8cf
Odos Matthews <omatth10@wgu.edu>
2025-06-30 15:52:07 -0400

    update docs

--- docs/_build/html/_sources/usage.rst.txt
+++ docs/_build/html/_sources/usage.rst.txt
@@ -1,8 +1,13 @@
 Usage Guide
 ===========
 
+Articuno converts Polars DataFrames into Pydantic models. Here's a basic example:
+
 .. code-block:: python
 
+    import polars as pl
     from articuno import df_to_pydantic
 
+    df = pl.DataFrame({"name": ["Alice"], "age": [30]})
     model = df_to_pydantic(df)
+    print(model.model_json_schema())
--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,20 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+project = "articuno"
+author = "Odos Matthews"
+release = "0.3.7"
 
-project = 'articuno'
-copyright = '2025, Odos Matthews'
-author = 'Odos Matthews'
-release = '0.3.7'
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+]
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+templates_path = ["_templates"]
+exclude_patterns = []
 
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
