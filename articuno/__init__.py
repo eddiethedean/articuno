e030a61a3197f0786daccca0ad288c5ed85e0987
Odo Matthews <odosmatthews@gmail.com>
2025-07-04 15:20:44 -0400

    fix init

--- articuno/__init__.py
+++ articuno/__init__.py
@@ -1,16 +1,6 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(".."))  # ensures your package is importable
+from .convert import df_to_pydantic, infer_pydantic_model
+from .codegen import generate_pydantic_class_code
 
-project = "articuno"
-author = "Odos Matthews"
-release = "0.3.8"
 
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",         # for Google/Numpy-style docstrings
-    "sphinx.ext.viewcode",
-    "sphinx_autodoc_typehints",    # for nice type hints
-]
-
-html_theme = "furo"  # or "sphinx_rtd_theme"
+__all__ = ["df_to_pydantic", "infer_pydantic_model", "generate_pydantic_class_code"]
+__version__ = "0.3.9"
